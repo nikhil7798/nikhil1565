1d37a91c52d9a4dfb581cc4626bbbd91ac67cf8a
nikhil7798 <nikhilpawar@gmail.com>
2022-04-21 13:54:23 +0530

    hi

---
+++ src/Demo1/D1.java
@@ -0,0 +1,15 @@
+package Demo1;
+
+public class D1 
+{
+  public void m1()
+  {
+	 System.out.println("Hey nik");
+  }
+	public static void main(String[] args) 
+	{
+      D1 a=new D1();
+      a.m1();
+	}
+
+}
---
+++ src/Demo1/D2.java
@@ -0,0 +1,21 @@
+package Demo1;
+
+public class D2 
+{
+	public void m1()
+	{
+		System.out.println("Nik");
+	}
+
+	public void m2()
+	{
+		System.out.println("Abhi");
+	}
+	public static void main(String[] args) 
+	{
+       D2 m=new D2();
+       m.m1();
+       m.m2();
+	}
+
+}
